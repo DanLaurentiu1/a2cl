5a9ede36c60e1f366eb0e06fb12cbab669b8aaff
DanLaurentiu <laurentiu.dan1@stud.ubbcluj.ro>
2025-05-07 20:13:15 +0300

    refactorings

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from app.api.routes.plans import router as plans_router
 from fastapi.middleware.cors import CORSMiddleware
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 app = FastAPI()
 app.include_router(plans_router)
-
 
 app.add_middleware(
     CORSMiddleware,
