5a9cf90685a93110efb308ed99b8756ca82418de
DanLaurentiu <laurentiu.dan1@stud.ubbcluj.ro>
2025-06-03 13:53:39 +0300

    update plan seeder

--- backend/app/core/database/seeds/PlanSeeder.py
+++ backend/app/core/database/seeds/PlanSeeder.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from app.core.database.models.DB_Profile import DB_Profile
 from app.core.database.models.DB_Plan import DB_Plan
 from app.core.database.models.DB_Problem import DB_Problem
+from app.core.database.models.DB_Topic import DB_Topic
 
 from typing import List, Dict
 
@@ -22,6 +23,9 @@ class PlanSeeder:
                     {"completed": True, "problem_id" : 8},
                     {"completed": False, "problem_id" : 12},
                     {"completed": False, "problem_id" : 18},
+                ],
+                "given_topics": [
+                    {"topic_id" : 3},
                 ]
             },
             {
@@ -37,6 +41,9 @@ class PlanSeeder:
                     {"completed": False, "problem_id" : 16},
                     {"completed": False, "problem_id" : 17},
                     {"completed": False, "problem_id" : 18},
+                ],
+                "given_topics": [
+                    {"topic_id" : 62},
                 ]
             },
             {
@@ -56,6 +63,9 @@ class PlanSeeder:
                     {"completed": False, "problem_id" : 27},
                     {"completed": False, "problem_id" : 28},
                     {"completed": False, "problem_id" : 29},
+                ],
+                "given_topics": [
+                    {"topic_id" : 14},
                 ]
             },
             {
@@ -67,6 +77,9 @@ class PlanSeeder:
                     {"completed": True, "problem_id" : 25},
                     {"completed": False, "problem_id" : 26},
                     {"completed": False, "problem_id" : 29},
+                ],
+                "given_topics": [
+                    {"topic_id" : 57},
                 ]
             }
         ]
@@ -75,6 +88,7 @@ class PlanSeeder:
     def run(db: Session):
         existing_plan_ids = {plan.id for plan in db.query(DB_Plan).all()}
         existing_problems_ids = {problem.id for problem in db.query(DB_Problem).all()}
+        existing_topics_ids = {topic.id for topic in db.query(DB_Topic).all()}
 
         for plan_data in PlanSeeder.seed_data():
             if plan_data["id"] not in existing_plan_ids:
@@ -89,12 +103,20 @@ class PlanSeeder:
                         print(f"Problem {problem_data['problem_id']} not found, skipping from plan {plan_data['id']}")
                         continue
                     valid_problems.append([problem_data["completed"], problem_data["problem_id"]])
-                
+
+                valid_topics = []
+                for topic_data in plan_data["given_topics"]:
+                    if topic_data["topic_id"] not in existing_topics_ids:
+                        print(f"Topic {topic_data['topic_id']} not found, skipping from plan {plan_data['id']}")
+                        continue
+                    valid_topics.append(topic_data["topic_id"])
+
                 db_plan = DB_Plan(
                     id=plan_data["id"],
                     title=plan_data["title"],
                     profile_id=profile.id,
-                    problems=valid_problems
+                    problems=valid_problems,
+                    given_topics=valid_topics
                 )
                 db.add(db_plan)
         db.commit()
