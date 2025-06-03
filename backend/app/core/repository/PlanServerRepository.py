01f7b47c0741b80c6994d86b788a0bf98b943041
DanLaurentiu <laurentiu.dan1@stud.ubbcluj.ro>
2025-06-03 17:13:17 +0300

    integrate actor-critic into backend problem decisions

--- backend/app/core/repository/PlanServerRepository.py
+++ backend/app/core/repository/PlanServerRepository.py
@@ -6,11 +6,13 @@ from app.core.database.models.DB_Plan import DB_Plan
 from app.core.database.models.DB_Problem import DB_Problem
 from app.core.database.models.DB_Profile import DB_Profile
 from app.core.database.models.DB_Topic import DB_Topic
+from app.model.Trainer import LeetcodeTrainer
 
 
 class PlanServerRepository:
     def __init__(self, db: Session):
         self._db = db
+        self._trainer: LeetcodeTrainer = LeetcodeTrainer()
     
     def get_all_plans(self) -> List[Plan]:
         db_plans: DB_Plan = self._db.query(DB_Plan).all()
@@ -23,14 +25,29 @@ class PlanServerRepository:
         db_plan: DB_Plan = self._db.query(DB_Plan).filter(DB_Plan.id == plan_id).first()
         return db_plan.to_entity(self._db)
     
-    def add_plan(self, plan_data: Plan) -> Plan:
+    def create_plan_with_recommendations(self, plan_data: Plan) -> Plan:
         plan_validator.validate_plan(plan_data)
+        plan_with_problems = self.add_problems_to_plan(plan_data)
+        print(5)
+        return self.add_plan(plan_with_problems)
 
+    def add_plan(self, plan_data: Plan) -> Plan:
         db_plan = DB_Plan.from_entity(plan_data, self._db, adding=True)
         self._db.add(db_plan)
         self._db.commit()
         self._db.refresh(db_plan)
         return db_plan.to_entity(self._db)
+    
+    def add_problems_to_plan(self, plan_data: Plan) -> Plan:
+        topic_names = [topic.name for topic in plan_data.given_topics]
+        recommended_problem_ids = self._trainer.evaluate(env_targets=topic_names, num_episodes=1, load_path="C:\\Users\\Lau\\.vscode\\projects\\thesis\\backend\\app\\model\\checkpoints\\final_model_500000.zip")
+        problems = []
+        for problem_id in recommended_problem_ids:
+            db_problem: DB_Problem = self._db.query(DB_Problem).get(problem_id)
+            if db_problem:
+                problems.append((False, db_problem.to_entity()))
+        plan_data.problems = problems
+        return plan_data
     
     def update_plan_problems(
         self, 
